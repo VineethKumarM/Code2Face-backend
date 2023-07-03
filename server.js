5780e58578b6d03b5d98f8d76f988295e69faaeb
Vineeth Kumar Munigyala <munigyala@gmail.com>
2023-07-03 19:20:09 +0530

    add slides routes

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express  = require('express')
 const app = express();
+const fs = require("fs");
+const path = require("path");
 const server = require('http').createServer(app)
 const cors  = require('cors')
 const ACTIONS = require('./actions.js');
@@ -17,10 +19,43 @@ app.use(cors({
     origin:'*',
     methods: ['GET','POST']
 }));
+app.use(express.json());
 
 const userSocketMap = {};
 const userPeer = {};
 const interviewers = {}; 
+
+app.post("/save_data", (req, res) => {
+    const dataFilePath = path.join(__dirname, "data.json");
+    let existingData = fs.readFileSync(dataFilePath, "utf8");
+    if(existingData.length==0) {
+        existingData= "[]"
+    }
+    const jsonData = JSON.parse(existingData);
+  
+
+    const { heading, body } = req.body;
+  
+    const newObject = {
+      heading: heading,
+      body: body
+    };
+  
+    jsonData.push(newObject);
+  
+    fs.writeFileSync(dataFilePath, JSON.stringify(jsonData), "utf8");
+  
+    res.sendStatus(200);
+});
+
+app.get("/get_data", (req, res) => {
+    const dataFilePath = path.join(__dirname, "data.json");
+    const existingData = fs.readFileSync(dataFilePath, "utf8");
+    const jsonData = JSON.parse(existingData);
+  
+    res.json(jsonData);
+});
+  
 
 function getAllConnectedClients(roomId) {
     return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map(
@@ -35,8 +70,10 @@ function getAllConnectedClients(roomId) {
 
 io.on('connection', (socket) => {
     let id1 = socket.id
+    console.log(id1);
 
     socket.on(ACTIONS.JOIN, ({ roomId, username, peerId, flag }) => {
+        console.log(username +'joined' + roomId);
         userSocketMap[socket.id] = username;
         userPeer[peerId] = username
         if(flag) {
